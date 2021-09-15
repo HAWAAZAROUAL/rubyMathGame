37f4c385048eb5e9997f448672447f6f09792819
Hawaa <hawaazaroual@gmail.com>
2021-09-15 07:53:15 -0600

    set up start game in game

--- game.rb
+++ game.rb
@@ -8,6 +8,19 @@ def initialize
   @current_player = nil
 end
 
+def play(p1, p2)
+  if winner
+    puts "Gameover! Winner is #{winner.name}."
+    return
+  end
+
+puts"-------Start the game!-----"
+puts"Player 1 (#{p1.name}) Vs Player 2(#{p2.name})"
+puts"----------------------------"
+
+end
+
+private 
 attr_accessor :current_player
 attr_writer :winner
 
