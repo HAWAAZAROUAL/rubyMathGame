916e8f459e5fe11860e5452cb4b53d1673e9901d
Hawaa <hawaazaroual@gmail.com>
2021-09-14 20:54:05 -0600

    finish planning, set up files, start the player class

---
+++ main.rb
@@ -0,0 +1,3 @@
+# require all other necessary files
+require_relative './game'
+require_relative'./player'
--- planning.rb
+++ planning.rb
@@ -1,2 +1,9 @@
 # The nouns I think can be made into classes:
-# player   question    numbers   lives    turns    math
+# class game : questions, and answer input to the math problem
+# class player : will have instances of player name, and current score/lives
+
+
+# Methods : 
+# one to make random numbers between 1 & 20 rand(1..20)
+# one to switch turns between players. Uses a function that will take in player 1 and player 2 as input
+# one that will check the score status, 
